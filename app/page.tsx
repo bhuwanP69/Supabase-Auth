3e0271710ee82b32fb45eec50170ebf851703204
Bhuwan <bhubanpaudel79@gmail.com>
2024-01-10 12:11:35 +0545

    remove apostopi because it show error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Navbar from './auth/layout'
 
 export default function page() {
@@ -6,7 +5,7 @@ export default function page() {
     <main>
         <Navbar/>
         <div className="first text-center pt-10">
-        <h2 className='text-2xl pb-10'>It's Just a first Page</h2>
+        <h2 className='text-2xl pb-10'>Its Just a first Page</h2>
          <p className='text-xl'>This is before the Login Page </p>
         </div>
     </main>
