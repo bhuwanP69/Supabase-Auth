dda292030fd48e9a5a18b163c4a057deabb70a3a
Bhuwan <bhubanpaudel79@gmail.com>
2024-01-10 12:44:31 +0545

    some change in get to nextRequest"

--- app/auth/auth/callback/route.tsx
+++ app/auth/auth/callback/route.tsx
@@ -1,8 +1,9 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export default async function Handler(request:any) {
+
+export default async function GET(request:NextRequest) {
     const url = new URL(request.url)
     const code = url.searchParams.get('code')
     if(code){
